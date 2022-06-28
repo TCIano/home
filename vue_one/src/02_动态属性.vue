3eb18f4316ecb646c6c59053b903772753eb540d
jin <2293748571@qq.com>
2022-06-28 20:13:11 +0800

    增加了动态属性

--- vue_one/src/02_动态属性.vue
+++ vue_one/src/02_动态属性.vue
@@ -1,23 +1,25 @@
 <template>
-  <div>
-    
+    <div>
+    <!-- 2. 值 -> 标签原生属性上 -->
+    <!-- 语法: v-bind:原生属性名="vue变量" -->
+    <a v-bind:href="url">点击去百度</a>
+    <!-- 语法: :原生属性名="vue变量" -->
+    <img :src="imgUrl" />
+    <img :src="localImg">
   </div>
 </template>
 
 <script>
-// http://yun.itheima.com/Upload/./Images/20210412/60741c11ab77b.jpg
+import Img from './assets/logo.png'
 export default {
-  name: 'VuecliDemo02',
+  name: 'VuecliDemo01',
 
   data() {
     return {
-      
+      url:'http://www.baidu.com',
+      img:"http://yun.itheima.com/Upload/./Images/20210412/60741c11ab77b.jpg",
+      localImg: Img
     };
-  },
-
-
-  methods: {
-    
-  },
+  }
 };
 </script>
--- vue_one/src/App.vue
+++ vue_one/src/App.vue
@@ -1,23 +1,24 @@
 <template>
-  <div>
-    <h1>{{msg}}</h1>
-    <h1>{{obj.name}}</h1>
-    <h1>{{obj.age}}</h1>
+    <div>
+    <!-- 2. 值 -> 标签原生属性上 -->
+    <!-- 语法: v-bind:原生属性名="vue变量" -->
+    <a v-bind:href="url">点击去百度</a>
+    <!-- 语法: :原生属性名="vue变量" -->
+    <img :src="imgUrl" />
+    <img :src="localImg">
   </div>
 </template>
 
 <script>
+import Img from './assets/logo.png'
 export default {
   name: 'VuecliDemo01',
 
   data() {
     return {
-      
-      msg:'Hello World',
-      obj:{
-        name:'zs',
-        age:18
-      }
+      url:'http://www.baidu.com',
+      img:"http://yun.itheima.com/Upload/./Images/20210412/60741c11ab77b.jpg",
+      localImg: Img
     };
   }
 };
