e29dd605f1c2e910e779421a7687350d721bfd2c
jin <2293748571@qq.com>
2022-06-28 20:31:01 +0800

    修饰符

--- vue_one/src/05_事件修饰符.vue
+++ vue_one/src/05_事件修饰符.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
-    <div>
-      <p>.stop - 阻止事件冒泡</p>
-      <a href="http://www.baidu.com" >去百度</a>
-      <p>点击观察事件处理函数执行几次</p>
+    <div @click="father">
+      <p @click.stop="fn">.stop - 阻止事件冒泡</p>
+      <a href="http://www.baidu.com" @click.prevent>去百度</a>
+      <p @click.once="once">点击观察事件处理函数执行几次</p>
     </div>
   </div>
 </template>
@@ -11,6 +11,15 @@
 <script>
 export default {
   methods: {
+    fn() {
+      console.log(11);
+    },
+    father() {
+      console.log('father');
+    },
+    once() {
+      console.log('one');
+    }
   }
 }
 </script>
--- vue_one/src/App.vue
+++ vue_one/src/App.vue
@@ -1,32 +1,29 @@
 <template>
   <div>
-    <!-- 1. 事件触发, 无传值, 可以直接获取事件对象
-    2. 事件触发, 传值, 需要手动传入$event -->
-    <a href="http://www.baidu.com" @click="Fn(10,$event)">百度</a>
-    <hr />
-    <a href="http://www.taobao.com">淘宝</a>
+    <div @click="father">
+      <p @click.stop="fn">.stop - 阻止事件冒泡</p>
+      <a href="http://www.baidu.com" @click.prevent>去百度</a>
+      <p @click.once="once">点击观察事件处理函数执行几次</p>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'VuecliDemo04',
-
-  data() {
-    return {
-      
-    };
-  },
-
   methods: {
-    Fn(num,e) {
-      console.log(num);
-      e.preventDefault()
+    fn() {
+      console.log(11);
+    },
+    father() {
+      console.log('father');
+    },
+    once() {
+      console.log('one');
     }
-  },
-};
+  }
+}
 </script>
 
-<style scoped>
+<style>
 
 </style>
