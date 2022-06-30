af4dc9e8cd7f61c9fdc425b756211b9b7d3e31c9
w17 <1050363049@qq.com>
2022-06-30 22:50:45 +0800

    案例：买书

--- zuoye_two/src/App.vue
+++ zuoye_two/src/App.vue
@@ -1,16 +1,29 @@
 <template>
-  <div>
-    <div
-    v-for="(item, index) in arr"
-    style =display:inline-block
-    :key="index">
-    <input 
-    type="checkbox"
-    v-model="checkNumArr" 
-    :value="item">
-    <span>{{item}}</span>
-    </div>
-    <p>你选中的元素之和为:{{theSum}}</p>
+<div>
+    <p>请选择你要购买的书籍</p>
+    <ul>
+      <li v-for="(item,ind) in arr" :key="ind">
+        <span>{{item['name']}}</span>
+        <button @click="buy(ind)">买书</button>
+      </li>
+    </ul>
+    <table border="1" width="500" cellspacing="0">
+      <tr>
+        <th>序号</th>
+        <th>书名</th>
+        <th>单价</th>
+        <th>数量</th>
+        <th>合计</th>
+      </tr>
+      <tr v-for="(item, index) in arr" :key="index">
+        <td>{{ index + 1}}</td>
+        <td>{{ item['name']}}</td>
+        <td>{{ item['price']}}</td>
+        <td>{{ item['count']}}</td>
+        <td>{{ item['price'] * item['count']}}</td>
+      </tr>
+    </table>
+    <p>总价格为: {{ allPrice }}</p>
   </div>
 </template>
 
@@ -18,18 +31,43 @@
 export default {
   data() {
     return {
-      arr: [9, 15, 19, 25, 29, 31, 48, 57, 62, 79, 87],
-      checkNumArr: [], //  保存用户选中的数字
+      arr: [
+        {
+          name: "水浒传",
+          price: 107,
+          count: 0,
+        },
+        {
+          name: "西游记",
+          price: 192,
+          count: 0,
+        },
+        {
+          name: "三国演义",
+          price: 219,
+          count: 0,
+        },
+        {
+          name: "红楼梦",
+          price: 178,
+          count: 0,
+        },
+      ],
     };
   },
+  methods: {
+    buy(index) {
+      this.arr[index]["count"]++;
+    },
+  },
   computed: {
-    theSum() {
-      return this.checkNumArr.reduce((sum, val) => {
-        return (sum += val)
-      },0)
-    }
-  }
-}
+    allPrice() {
+      return this.arr.reduce((sum, obj) => {
+        return (sum += obj["price"] * obj["count"]);
+      }, 0);
+    },
+  },
+};
 </script>
 
 <style>
