c1cbc7b8840caf625235929ca8e949901d647dd9
zqxtyl <2571756029@qq.com>
2022-06-30 20:54:28 +0800

    完成6-30

---
+++ 6-30/数字‘.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <ul>
+      <li v-for="(item, ind) in arr" :key="item">
+        <span>{{ item }}</span>
+        <button @click="del(ind)">删除</button>
+      </li>
+    </ul>
+    <button @click="add">生成</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      arr: [1, 5, 3],
+    };
+  },
+  methods: {
+    del(index){
+        this.arr.splice(index,1)
+    },
+    add(){
+        this.arr.push(Math.floor(Math.random()*20))
+    }
+  },
+};
+</script>
+
+<style></style>
---
+++ 6-30/走一走.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <ul>
+        <li v-for="item in str" :key="item">{{item}}</li>
+    </ul>
+    <button @click="zou">走</button>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'',
+    data(){
+        return{
+            str:['帅哥','美女','程序员']
+        }
+    },
+    methods: {
+        zou(){
+            this.str.push(this.str[0])
+            this.str.shift()
+        }
+    },
+
+}
+</script>
+
+<style>
+
+</style>
