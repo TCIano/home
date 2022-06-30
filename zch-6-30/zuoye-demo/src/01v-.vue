a406c0d624797a3a37a274444c5bb6449ca87b01
Mynmhui <286734637@qq.com>
2022-06-30 20:03:00 +0800

    v-for的更新检测

---
+++ zch-6-30/zuoye-demo/src/01v-.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+  <ul>
+    <li v-for="item in arr" :key="item">{{item}}</li>
+    <li v-for="(item, index) in arr" :key="index">{{item}}==={{index}}</li>
+  </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+    data() {
+      return {
+        arr: ["小明", "小欢欢", "大黄"],
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
