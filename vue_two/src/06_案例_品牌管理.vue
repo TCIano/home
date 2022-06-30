2662bc3ff3040b9eac6f955ecf8696c916105334
jin <2293748571@qq.com>
2022-06-30 20:06:47 +0800

    完成品牌铺设 新增和铺设

--- vue_two/src/06_案例_品牌管理.vue
+++ vue_two/src/06_案例_品牌管理.vue
@@ -20,14 +20,14 @@
           </tr>
         </thead>
         <tbody>
-          <tr v-for="item in list" :key="item.id">
-            <td>{{item.id}}</td>
-            <td>{{item.name}}</td>
+          <tr >
+            <td></td>
+            <td></td>
 
             <!-- 如果价格超过100，就有red这个类 -->
-            <td :class="{red:item.price>100}">{{item.price}}</td>
-            <td>{{item.time}}</td>
-            <td><a href="#" @click="delFn(item.id)">删除</a></td>
+            <td class="red"></td>
+            <td></td>
+            <td><a href="#" >删除</a></td>
           </tr>
           <!-- <tr style="background-color: #EEE">
               <td>统计:</td>
@@ -35,7 +35,7 @@
               <td colspan="2">平均价: 0</td>
           </tr> -->
         </tbody>
-        <!-- 
+          <!-- 
         <tfoot >
           <tr>
             <td colspan="5" style="text-align: center">暂无数据</td>
@@ -48,18 +48,26 @@
       <form class="form-inline" style="display:flex;">
         <div class="form-group">
           <div class="input-group">
-            <input type="text" class="form-control" placeholder="资产名称" v-model.trim="name" />
+            <input
+              type="text"
+              class="form-control"
+              placeholder="资产名称"
+            />
           </div>
         </div>
         &nbsp;&nbsp;&nbsp;&nbsp;
         <div class="form-group">
           <div class="input-group">
-            <input type="text" class="form-control" placeholder="价格" v-model.trim="price" />
+            <input
+              type="text"
+              class="form-control"
+              placeholder="价格"
+            />
           </div>
         </div>
         &nbsp;&nbsp;&nbsp;&nbsp;
         <!-- 阻止表单提交 -->
-        <button class="btn btn-primary" @click.prevent="btn">添加资产</button>
+        <button class="btn btn-primary">添加资产</button>
       </form>
     </div>
   </div>
@@ -75,42 +83,19 @@ export default {
   data() {
     return {
       list: [
-        { id: 100, name: "外套", price: 199, time: new Date("2010-08-12") },
-        { id: 101, name: "裤子", price: 34, time: new Date("2013-09-01") },
-        { id: 102, name: "鞋", price: 25.4, time: new Date("2018-11-22") },
-        { id: 103, name: "头发", price: 19900, time: new Date("2020-12-12") },
+        { id: 100, name: "外套", price: 199, time: new Date('2010-08-12')},
+        { id: 101, name: "裤子", price: 34, time: new Date('2013-09-01') },
+        { id: 102, name: "鞋", price: 25.4, time: new Date('2018-11-22') },
+        { id: 103, name: "头发", price: 19900, time: new Date('2020-12-12') }
       ],
-      name: "",
-      price: 0,
     };
   },
-  methods: {
-    btn() {
-      if (this.name == "" && this.price === "0") {
-        return alert("请输入完整");
-      }
 
-      const id = this.list[this.list.length - 1]
-        ? this.list[this.list.length - 1].id + 1
-        : 100;
-      this.list.push({
-        id,
-        name: this.name,
-        price: this.price,
-        time: new Date(),
-      });
-    },
-    delFn(id) {
-      const index = this.list.findIndex((item) => id == item.id);
-      console.log(index);
-      this.list.splice(index, 1);
-    },
-  },
 };
 </script>
 
 <style >
-.red {
+.red{
   color: red;
 }
 </style>
