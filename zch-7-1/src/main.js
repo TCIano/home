2a0b91f5dc4add88336b7918260ed0db4e71da60
Mynmhui <286734637@qq.com>
2022-07-02 16:13:19 +0800

    案例-品牌管理(数据缓存)

--- zch-7-1/src/main.js
+++ zch-7-1/src/main.js
@@ -4,6 +4,7 @@ import App from './App.vue'
 Vue.config.productionTip = false
 
 import "bootstrap/dist/css/bootstrap.css"
+ // 默认找文件夹下的index文件(但是这个不是所以需要写路径)
 
 Vue.filter('toReverse', (val,splits) => {
   return val.split('').reverse().join(splits || '')
