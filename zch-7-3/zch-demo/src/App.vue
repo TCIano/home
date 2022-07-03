07760bd8fd9f90651d0c0cc3c6823eddb999ab40
Mynmhui <286734637@qq.com>
2022-07-03 22:15:04 +0800

    案例-数据缓存

--- zch-7-3/zch-demo/src/App.vue
+++ zch-7-3/zch-demo/src/App.vue
@@ -20,11 +20,12 @@ import TodoFooter from "./components/TodoFooter";
 export default {
   data() {
     return {
-      list: [
-        { id: 100, name: "吃饭", isDone: true },
-        { id: 101, name: "睡觉", isDone: false },
-        { id: 102, name: "打豆豆", isDone: true },
-      ],
+      // list: [
+      //   { id: 100, name: "吃饭", isDone: true },
+      //   { id: 101, name: "睡觉", isDone: false },
+      //   { id: 102, name: "打豆豆", isDone: true },
+      // ],
+      list: JSON.parse(localStorage.getItem("list")) || [],
       getSel: "all",
     }
   },
@@ -65,6 +66,13 @@ export default {
         return this.list.filter((ele) => ele.isDone)
     }else{
       return this.list;
+    }
+  }
+},
+watch: {
+  list: {
+    handler(val){
+      localStorage.setItem("list", JSON.stringify(val || []));
     }
   }
 }
