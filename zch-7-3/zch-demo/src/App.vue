0c48701cdddede117682072fe1da50e143077e19
Mynmhui <286734637@qq.com>
2022-07-03 21:00:03 +0800

    案例-铺设待办任务

--- zch-7-3/zch-demo/src/App.vue
+++ zch-7-3/zch-demo/src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <section class="todoapp">
     <!-- 除了驼峰, 还可以使用-转换链接 -->
-    <TodoHeader></TodoHeader>
-    <TodoMain></TodoMain>
+    <TodoHeader ></TodoHeader>
+    <TodoMain :list="list"></TodoMain>
     <TodoFooter></TodoFooter>
   </section>
 </template>
@@ -15,6 +15,15 @@ import TodoFooter from "./components/TodoFooter";
 
 
 export default {
+  data() {
+    return {
+      list: [
+        { id: 100, name: "吃饭", isDone: true },
+        { id: 101, name: "睡觉", isDone: false },
+        { id: 102, name: "打豆豆", isDone: true },
+      ],
+    }
+  },
   components: {
     TodoHeader,
     TodoMain,
--- zch-7-3/zch-demo/src/components/TodoMain.vue
+++ zch-7-3/zch-demo/src/components/TodoMain.vue
@@ -1,11 +1,15 @@
 <template>
   <ul class="todo-list">
     <!-- completed: 完成的类名 -->
-    <li class="completed" >
+    <li :class="{completed: item.isDone}" 
+    v-for="item in list" :key="item.id"
+    >
       <div class="view">
-        <input class="toggle" type="checkbox" />
-        <label>任务名</label>
-        <button class="destroy"></button>
+        <input class="toggle" type="checkbox" 
+        v-model="item.isDone"
+        />
+        <label>{{ item.name }}</label>
+        <button class="destroy" @click="delFn(item.id)"></button>
       </div>
     </li>
   </ul>
@@ -14,5 +18,6 @@
 
 <script>
 export default {
+    props: ["list"]
 }
 </script>
