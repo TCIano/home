b57693af994f0c77ac925f7f5968b42a21a685b5
Mynmhui <286734637@qq.com>
2022-07-05 19:17:10 +0800

    axios基本使用

--- zch-7-4/src/App.vue
+++ zch-7-4/src/App.vue
@@ -2,14 +2,20 @@
   <div>
     <Life v-if="show"></Life>
     <button @click="show = false">销毁组件</button>
+    <br>
+    <h1>2. axios使用</h1>
+    <UseAxios></UseAxios>
+    <hr>
   </div>
 </template>
 
 <script>
 import Life from './components/Life.vue'
+import UseAxios from './components/UseAxios.vue'
 export default {
   components: {
     Life,
+    UseAxios,
   },
   data() {
     return {
