90354c7f513b1fdba6e0f45400facc048f6e0987
Mynmhui <286734637@qq.com>
2022-07-05 18:51:31 +0800

    生命周期

--- zch-7-4/src/App.vue
+++ zch-7-4/src/App.vue
@@ -1,84 +1,22 @@
 <template>
-<div>
-<SearchBook @find="findBook"></SearchBook>
-<BodyBook :list="list" @del="delFn" @detail="detailFn"></BodyBook>
-<AddBook v-if="isShow" @add="addBook" ref="addbook"></AddBook>
-</div>
+  <div>
+    <Life v-if="show"></Life>
+    <button @click="show = false">销毁组件</button>
+  </div>
 </template>
 
 <script>
-import SearchBook from './components/SearchBook.vue'
-import BodyBook from './components/BodyBook.vue'
-import AddBook from './components/AddBook.vue'
-  export default {
-    data(){
-      return {
-        list: [],
-        isShow: true,
-      }
-    },
-    components: {
-      SearchBook,
-      BodyBook,
-      AddBook,
-    },
-    created() {
-      this.getList();
-    },
-    methods: {
-    findBook() {
-      const params = {};
-      this.bookname ? (params.bookname = this.bookname) : "";
-      this.getList(params)
-    },
-      getList(params = {}) {
-      this.$axios({
-        url: "/api/getbooks",
-        params: params,
-      }).then((res) => {
-        this.list = res.data.data;
-      });
-    },
-      addBook(bookObj) {
-        this.$refs.addbook.disabled = true;
-        this.$axios({
-        url: '/api/addbook',
-        method: 'POST',
-        data: {
-          ...bookObj,
-          appkey: "7250d3eb-18e1-41bc-8bb2-11483665535a",
-        },
-      }).then((res) => {
-        this.$refs.addbook.disabled = false;
-        alert(res.data.msg)
-        if(res.data.status === 200 || res.data.status === 201) {
-            this.getList();
-        }
-      })
-    },
-      delFn(id){
-        this.$axios({
-          url: '/api/delbook',
-          params: {id},
-        }).then(() => {
-          this.getList();
-        })
-      },
-      detailFn(id) {
-      this.$axios({
-        url: "/api/getbooks",
-        params: { id },
-      }).then((res) => {
-        const info = res.data.data && res.data.data[0];
-        alert(
-          `作者：${info.author}; 出版社：${info.publisher}; 书名：${info.bookname}`
-        );
-      });
-    },
+import Life from './components/Life.vue'
+export default {
+  components: {
+    Life,
+  },
+  data() {
+    return {
+      show: true,
     }
   }
+}
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style scoped></style>
--- zch-7-4/src/main.js
+++ zch-7-4/src/main.js
@@ -3,9 +3,9 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
-import axios from "axios";
-axios.defaults.baseURL = "http://www.liulongbin.top:3006";
-Vue.prototype.$axios = axios;
+// import axios from "axios";
+// axios.defaults.baseURL = "http://www.liulongbin.top:3006";
+// Vue.prototype.$axios = axios;
 
 new Vue({
   render: h => h(App),
