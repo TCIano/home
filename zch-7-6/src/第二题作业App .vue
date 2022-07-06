890fd624d5dd4709d71d6ff3acc120c3324b947c
Mynmhui <286734637@qq.com>
2022-07-07 00:19:24 +0800

    作业第二题

---
+++ zch-7-6/src/第二题作业App .vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+  <UserTable>
+    <template v-slot:id="scope">
+      <input v-model="scope.row.id">
+    </template>
+        <template v-slot:name="scope">
+      <input v-model="scope.row.name">
+    </template>
+        <template v-slot:age="scope">
+      <input v-model="scope.row.age">
+    </template>
+        <template v-slot:count="scope">
+      <img :src="scope.row.headImgUrl" alt="" width="60px">
+    </template>
+  </UserTable>
+  </div>
+</template>
+
+<script>
+import UserTable from './views/07_UserTable.vue'
+  export default {
+    components: {
+      UserTable
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
