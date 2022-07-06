c44af45a8967e2a2f2d7988f0400719be3de37aa
Mynmhui <286734637@qq.com>
2022-07-07 00:18:28 +0800

    作业第二题

--- zch-7-6/src/App.vue
+++ zch-7-6/src/App.vue
@@ -1,21 +1,27 @@
 <template>
-<div>
-  <page></page>
-</div>
+  <div>
+  <UserTable>
+    <template v-slot:id="scope">
+      <input v-model="scope.row.id">
+    </template>
+        <template v-slot:name="scope">
+      <input v-model="scope.row.name">
+    </template>
+        <template v-slot:age="scope">
+      <input v-model="scope.row.age">
+    </template>
+        <template v-slot:count="scope">
+      <img :src="scope.row.headImgUrl" alt="" width="60px">
+    </template>
+  </UserTable>
+  </div>
 </template>
 
 <script>
-import page from './views/01_page.vue'
-// import emaile from './components/registered/emaile.vue'
+import UserTable from './views/07_UserTable.vue'
   export default {
     components: {
-      page,
-      // emaile,
-    },
-    data() {
-      return{
-        comName: 'page',
-      }
+      UserTable
     }
   }
 </script>
---
+++ zch-7-6/src/第一题作业App.vue
@@ -0,0 +1,25 @@
+<template>
+<div>
+  <page></page>
+</div>
+</template>
+
+<script>
+import page from './views/01_page.vue'
+// import emaile from './components/registered/emaile.vue'
+  export default {
+    components: {
+      page,
+      // emaile,
+    },
+    data() {
+      return{
+        comName: 'page',
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
