29f925f6e6a98e2c40aa33d3980acaf9db99d776
w17 <1050363049@qq.com>
2022-07-06 21:08:57 +0800

    Tab栏切换

---
+++ zuoye_six/src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <a href="#" @click="comName = 'Tel'">手机注册</a>
+    <a href="#" @click="comName = 'Email'">邮箱注册</a>
+    <div style="border: 5px solid #a3cf62">
+      <keep-alive>
+        <component :is="comName"> </component>
+      </keep-alive>
+    </div>
+  </div>
+</template>
+
+<script>
+import Tel from "./components/01/UserInfo.vue";
+import Email from "./components/01/UserName.vue";
+
+export default {
+  data() {
+    return {
+      comName: "Tel",
+    };
+  },
+  components: {
+    Tel,
+    Email,
+  },
+};
+</script>
+
+<style>
+a {
+  color: #7fb80e;
+  margin: 50px;
+  text-decoration: none;
+}
+.inputborder {
+  width: 200px;
+  height: 40px;
+  border: 2px solid green;
+  line-height: 40px;
+  margin: 10px 60px;
+  padding-left: 10px;
+}
+.btn {
+  width: 220px;
+  height: 40px;
+  background-color: #b2d235;
+  border: 2px solid green;
+  line-height: 40px;
+  margin: 20px 60px;
+}
+</style>
---
+++ zuoye_six/src/components/01/UserInfo.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <span>选择所在地区（实名认证主体所在地）</span><br />
+    <input type="checkbox" class="inputcheckbox" />中国大陆
+    <input type="checkbox" class="inputcheckbox" />中国香港
+    <span>手机号码</span>
+    <input type="text" placeholder="输入手机号码" class="inputborder" />
+    <input type="text" placeholder="输入验证码" class="inputborder1" />
+    <button class="btn1">发送验证码</button><br />
+    <span>登录密码</span>
+    <input type="text" placeholder="输入密码" class="inputborder" />
+    <button class="btn">注册</button>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      list: [],
+    };
+  },
+};
+</script>
+<style>
+span {
+  font-size: 12px;
+  margin: 60px;
+  color: green;
+}
+.inputcheckbox {
+  margin-left: 55px;
+}
+.inputborder1 {
+  width: 100px;
+  height: 40px;
+  border: 2px solid green;
+  line-height: 40px;
+  margin: 10px 15px 10px 60px;
+  padding-left: 10px;
+}
+.btn1 {
+  height: 45px;
+  background-color: #b2d235;
+  border: 2px solid green;
+  line-height: 40px;
+}
+</style>
