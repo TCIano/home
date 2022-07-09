9a47d749660a350047fe7b062e8ff5ca3e008eed
Mynmhui <286734637@qq.com>
2022-07-09 19:15:39 +0800

    路由的使用

---
+++ zch-7-9/src/main.js
@@ -0,0 +1,35 @@
+import Vue from 'vue'
+import App from './App.vue'
+import Find from '@/views/Find' // @是src的绝对地址
+import My from '@/views/My.vue'
+import Part from '@/views/Part.vue'
+import NotFound from '@/views/NotFound.vue'
+// import Recommend from '@/views/Second/Recommend'
+// import Ranking from '@/views/Second/Ranking'
+// import SongList from '@/views/Second/SongList'
+// 目标: vue-router基础使用
+// 1. 下载vue-router  (yarn add vue-router)
+// 2. 引入
+import VueRouter from 'vue-router'
+// 3. 注册全局组件
+Vue.use(VueRouter)
+// 4. 规则数组
+const routes = [
+  { path: '/find',component: Find},
+  { path: '/my',component: My},
+  { path: '/part',component: Part},
+  {path: '*', component: NotFound}
+]
+// 5. 生成路由对象
+const router = new VueRouter({
+  routes,
+  // mode: 'history',
+})
+
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  render: h => h(App),
+}).$mount('#app')
---
+++ zch-7-9/src/views/Second/SongList.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <!-- <p>推荐</p>
+    <p>排行榜</p>
+    <p>歌单</p> -->
+    <div class="nav_main">
+      <router-link to="/find/songlist/more">歌单</router-link>
+    </div>
+
+    <div style="1px solid red;">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
