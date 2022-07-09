2ab3f078d766db6aa3da1e02fb652ee0d3cc2070
Mynmhui <286734637@qq.com>
2022-07-09 22:12:43 +0800

    附加练习_1.切换页面

---
+++ zch-7-9/text1-demo/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div>
+      <router-link to="page">首页</router-link>
+      <router-link to="classify">分类</router-link>
+      <router-link to="indent">订单</router-link>
+      <router-link to="my1">我的</router-link>
+    </div>
+    <div>
+      <p>显示路由切换的页面</p>
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped></style>
--- zch-7-9/text1-demo/src/main.js
+++ zch-7-9/text1-demo/src/main.js
@@ -1,8 +1,33 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Page from '@/views/Page.vue'
+import Classify from '@/views/Classify.vue'
+import Indent from '@/views/Indent.vue'
+import My1 from '@/views/My1.vue'
+import NotFound from '@/views/NotFound.vue'
+// 引入
+import VueRouter from 'vue-router'
+
+// 注册全局组件
+Vue.use(VueRouter)
+// 规则数组
+const routes = [
+  {path: '/', redirect: "/page"},
+  {path: '/page', component: Page},
+  {path: '/classify', component: Classify},
+  {path: '/indent', component: Indent},
+  {path: '/my1', component: My1},
+  { path: '*', component: NotFound}
+]
+// 生成路由对象
+const router = new VueRouter({
+  routes,
+})
+
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
