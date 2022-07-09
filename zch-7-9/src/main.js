70ba7fb53e08bb7c6996f5c9c76e1638390c14ec
Mynmhui <286734637@qq.com>
2022-07-09 19:18:51 +0800

    路由 - 重定向

--- zch-7-9/src/main.js
+++ zch-7-9/src/main.js
@@ -4,9 +4,9 @@ import Find from '@/views/Find' // @是src的绝对地址
 import My from '@/views/My.vue'
 import Part from '@/views/Part.vue'
 import NotFound from '@/views/NotFound.vue'
-// import Recommend from '@/views/Second/Recommend'
-// import Ranking from '@/views/Second/Ranking'
-// import SongList from '@/views/Second/SongList'
+import Recommend from '@/views/Second/Recommend'
+import Ranking from '@/views/Second/Ranking'
+import SongList from '@/views/Second/SongList'
 // 目标: vue-router基础使用
 // 1. 下载vue-router  (yarn add vue-router)
 // 2. 引入
@@ -15,6 +15,7 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 // 4. 规则数组
 const routes = [
+  {path: '/', redirect: "/find"},
   { path: '/find',component: Find},
   { path: '/my',component: My},
   { path: '/part',component: Part},
